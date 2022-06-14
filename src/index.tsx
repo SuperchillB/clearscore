e564eeab44a971bf623e13f12888cdd480770e8b
Bertrand Honner <honnerbertrand@gmail.com>
2022-06-14 17:26:31 +0200

    Create entry file + App component

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import './index.scss';
+import App from "./App";
+// import Preloader from './components/Preloader';
+
+const container = document.getElementById("app");
+const app = createRoot(container!);
+
+app.render(
+  <React.StrictMode>
+    {/* <Suspense fallback={<Preloader />}> */}
+    <App />
+    {/* </Suspense> */}
+  </React.StrictMode>
+);
