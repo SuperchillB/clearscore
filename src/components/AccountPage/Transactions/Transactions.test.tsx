45e6d11e083708126705f191549ad49e0df1b411
Bertrand Honner <honnerbertrand@gmail.com>
2022-06-16 22:14:47 +0200

    Write more tests for Transactions component

--- src/components/AccountPage/Transactions/Transactions.test.tsx
+++ src/components/AccountPage/Transactions/Transactions.test.tsx
@@ -1,6 +1,7 @@
 import { screen as screenRTL, waitFor } from '@testing-library/react';
 import Transactions from '.';
 import { renderGetAccountHook, renderWithClient } from '../../../tests/utils';
+import Account from '../../../tests/fixtures/account.json';
 
 describe('Transactions feature component', () => {
   test('loads correctly with successful query', async () => {
@@ -39,6 +40,24 @@ describe('Transactions feature component', () => {
       await componentResult.getByTestId('transaction-cards-container').children
         .length,
     ).toBe(20);
+    const element = screenRTL.getAllByTestId('transaction-card');
+    expect(element).toBeDefined();
+  });
+
+  test('renders skeletons when loading', async () => {
+    renderWithClient(<Transactions isLoading={true} transactions={[]} />);
+
+    const skeleton = screenRTL.queryByTestId('transactions-skeleton');
+    expect(skeleton).toBeInTheDocument();
+  });
+
+  test('hides skeletons when fetching is complete', async () => {
+    renderWithClient(
+      <Transactions isLoading={false} transactions={Account.transactions} />,
+    );
+
+    const skeleton = screenRTL.queryByTestId('transactions-skeleton');
+    expect(skeleton).not.toBeInTheDocument();
     const element = screenRTL.getAllByTestId('transaction-card');
     expect(element).toBeDefined();
   });
