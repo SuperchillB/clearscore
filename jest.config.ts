5d0eaaa55acc17fbc4181faddff63f27a2da8b14
Bertrand Honner <honnerbertrand@gmail.com>
2022-06-16 19:39:52 +0200

    Add documentation for Jest external styles import

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '\\.(css|less|scss)$': '<rootDir>/src/__mocks__/styleMock.js', // This will make sure Jest will not import any external CSS/SCSS files in our tests
   },
   testTimeout: 50000,
 };
--- src/__mocks__/styleMock.js
+++ src/__mocks__/styleMock.js
@@ -1,1 +1,9 @@
+// We're exporting here an empty module to make sure Jest doesn't throw
+// any errors for components that import external resources that are not
+// JavaScript files (such as css/scss files.
+// A good example for this is in App.tsx where we are importing
+// 'react-loading-skeleton/dist/skeleton.css'. Jest treats this import as
+// a JS import, hence the mock file we've created here for styles. This,
+// along with the "moduleNameMapper" used in the Jest config, tells Jest
+// not to import any external css/scss but do use this file instead.
 exports.module = {};
